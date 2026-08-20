298acadb8785073a2706f04649d025c02cfcad16
Guilherme Garcia <guigarciamonteiro@gmail.com>
2026-08-20 10:54:53 -0300

    feat(core): add hash structure

---
+++ core/hashing.py
@@ -0,0 +1,15 @@
+class IndexHashStatic:
+    def __init__(self):
+        pass
+
+    def insert(key, value):
+        pass
+
+    def search(key):
+        pass
+
+    def delete(key):
+        pass
+
+    def hash():
+        pass
