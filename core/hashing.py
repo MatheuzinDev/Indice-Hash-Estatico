ca42065655b6c4eb72af8e78d54f9988163a7586
Guilherme Garcia <guigarciamonteiro@gmail.com>
2026-08-23 15:17:45 -0300

    feat(core): implement hash function and improve bucket population logic in IndexHashStatic

--- core/hashing.py
+++ core/hashing.py
@@ -1,11 +1,15 @@
+import math
+
 class IndexHashStatic:
     def __init__(self, data: list[str]):
-        self.NR = len(data)
-        self.FR = self._validate_FR(int(self.NR * 0.50))
-        self.NB = self._validate_NB((self.NR // self.FR) + 1)
-        self.SIZE = 0
+        self.NR = len(data) + 1
+        self.FR = self._validate_FR(20)
+        self.COLISION = 0
+        ocupacao_alvo = 0.70
+        self.NB = self._validate_NB(math.ceil(self.NR / (self.FR * ocupacao_alvo)))
         self.bucket = self._init_bucket(self.NB, self.FR, data)
         print(f"Sistema inicializado: Foram criados {self.NB} buckets (NB) com capacidade {self.FR} (FR).")
+        print(f"Total de palavras armazenadas: {self.NR-1}. Total de colisões: {self.COLISION}.")
 
     def _validate_FR(self, num: int):
         return num or 1
@@ -23,21 +27,70 @@ class IndexHashStatic:
     def _dinamic_bucket(self, NB: int, FR: int):
         bucket = [None] * NB
         for i in range(0, NB):
-             bucket[i] = [[None, i] for _ in range(FR)]
+             bucket[i] = [[None, -1] for _ in range(FR)]
         return bucket
 
     def _population_bucket(self, bucket: list[None], data: list[str]):
-        #TODO PRECISA CRIAR A FUNÇÃO HASH ANTES DE POPULAR O BUCKET
+        fr = self.FR
+        local_size = 0
+        hash_func = self.hash
+        next_free_pos = [0] * self.NB
+
+        for word in data:
+            key = hash_func(word)
+            pos = next_free_pos[key]
+            if pos < fr:
+                bucket[key][pos][0] = word
+                bucket[key][pos][1] = local_size + 1
+                next_free_pos[key] += 1 
+                local_size += 1
+            else:
+                print(f"Alerta: Bucket {key} lotou na palavra '{word}'.")
+                self.COLISION += 1
+            
         return bucket
 
-    def insert(self, key, value):
-        pass
+    def insert(self, value: str):
+        key = self.hash(value)
+        for bucket_pos in range(self.FR):
+            if self.bucket[key][bucket_pos][0] is None: 
+                self.bucket[key][bucket_pos][0] = value
+                self.bucket[key][bucket_pos][1] = self.SIZE + 1
+                self.SIZE = self.SIZE + 1
+                return True
+        print(f"Colisão máxima atingida! O bucket {key} está cheio.")
+        self.COLISION += 1
+        return False
 
-    def search(self, key):
-        pass
+    def search(self, value: str):
+        key = self.hash(value)
+        for bucket_pos in range(self.FR):
+            slot_value = self.bucket[key][bucket_pos][0]
+            if slot_value == value:
+                return self.bucket[key][bucket_pos] 
+            if slot_value is None:
+                return None 
+        return None
 
     def delete(self, key):
         pass
 
-    def hash(self):
-        pass
+    def hash(self, value: str):
+        hash = 5381
+        for char in value:
+            hash *= 33 + ord(char)
+        return hash % self.NB
+
+"""
+Como usuário
+Quero que o índice utilize uma função hash definida pela equipe
+Para mapear chaves de busca em buckets.
+
+Regras de Negócio
+RN10: A função hash deve mapear uma chave em um endereço de bucket.
+RN11: A função hash deve ser escolhida/projetada pela equipe.
+
+Critérios de Aceitação
+CA11: Dada uma chave, o sistema retorna sempre o mesmo bucket.
+CA12: A função hash sempre retorna um bucket dentro do intervalo válido [0..NB-1].
+"""
