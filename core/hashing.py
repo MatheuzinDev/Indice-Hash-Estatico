397628fa6790d46bd3a2678e6d44765990777173
Guilherme Garcia <guigarciamonteiro@gmail.com>
2026-08-23 11:52:48 -0300

    refactor: remove outdated comments from IndexHashStatic class

--- core/hashing.py
+++ core/hashing.py
@@ -1,19 +1,3 @@
-"""
-Como usuário
-Quero que o sistema crie automaticamente os buckets do índice
-Para permitir a construção do índice hash estático.
-
-Regras de Negócio
-RN08: O número de buckets NB deve obedecer:
-• NB > NR / FR, onde NR é o número de registros e FR é o tamanho do bucket.
-RN09: FR (capacidade do bucket) deve ser definido pela equipe.
-
-Critérios de Aceitação
-CA08: O sistema calcula e exibe NB.
-CA09: O sistema cria NB buckets com capacidade FR.
-CA10: O sistema impede NB <= NR/FR.
-"""
-
 class IndexHashStatic:
     def __init__(self, data: list[str]):
         self.NR = len(data)
